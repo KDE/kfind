8b96c06a4d228b6031961aa1735ac6eea2d552ee
Chusslove Illich <caslav.ilic@gmx.net>
2007-07-02 08:49:20 +0000

    Interface modifications to KCmdLineArgs, KAboutData and KComponentData; BIC, SIC. Most of the depending code has been auto-converted to new API.
    svn path=/trunk/KDE/kdebase/apps/; revision=682259

--- kappfinder/main.cpp
+++ kappfinder/main.cpp
@@ -26,20 +26,18 @@
 
 static const char description[] = I18N_NOOP( "KDE's application finder" );
 
-static KCmdLineOptions options[] = {
-  { "dir <dir>", I18N_NOOP( "Install .desktop files into directory <dir>" ), 0 },
-  KCmdLineLastOption
-};
-
 int main( int argc, char *argv[] )
 {
-  KAboutData aboutData( "kappfinder", I18N_NOOP( "KAppfinder" ),
-                        "1.0", description, KAboutData::License_GPL,
-                        "(c) 1998-2000, Matthias Hoelzer-Kluepfel" );
-  aboutData.addAuthor( "Matthias Hoelzer-Kluepfel", 0, "hoelzer@kde.org" );
-  aboutData.addAuthor( "Tobias Koenig", 0, "tokoe@kde.org" );
+  KAboutData aboutData( "kappfinder", 0, ki18n( "KAppfinder" ),
+                        "1.0", ki18n(description), KAboutData::License_GPL,
+                        ki18n("(c) 1998-2000, Matthias Hoelzer-Kluepfel") );
+  aboutData.addAuthor( ki18n("Matthias Hoelzer-Kluepfel"), KLocalizedString(), "hoelzer@kde.org" );
+  aboutData.addAuthor( ki18n("Tobias Koenig"), KLocalizedString(), "tokoe@kde.org" );
 
   KCmdLineArgs::init( argc, argv, &aboutData );
+
+  KCmdLineOptions options;
+  options.add("dir <dir>", ki18n( "Install .desktop files into directory <dir>" ));
   KCmdLineArgs::addCmdLineOptions( options );
   KCmdLineArgs *args = KCmdLineArgs::parsedArgs();
 
