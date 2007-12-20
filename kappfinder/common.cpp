f5273a96977ddfee1b8dab02cd4f48098c68974b
Tom Albers <toma@kde.org>
2007-12-20 22:23:07 +0000

    RELICENSE: GPL2->GPL2+/GPL23
    svn path=/trunk/KDE/kdebase/apps/; revision=751022

--- kappfinder/common.cpp
+++ kappfinder/common.cpp
@@ -6,8 +6,9 @@
     Based on code written by Matthias Hoelzer-Kluepfel <hoelzer@kde.org>
 
     This program is free software; you can redistribute it and/or
-    modify it under the terms of version 2 of the GNU General Public
-    License as published by the Free Software Foundation.
+    modify it under the terms of the GNU General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
