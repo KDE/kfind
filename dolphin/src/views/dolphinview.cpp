KDE/kde-baseapps

commit b25059e803e7eed54f0593605a4066882d63bce2

Fix Bug 217575 - Auto select pasted files doesn't select all files if some have been skipped
BUG: 217575
REVIEW: 107237
FIXED-IN: 4.9.4

dolphin/src/views/dolphinview.cpp
@@ -598,6 +598,8 @@ void DolphinView::setUrl(const KUrl& url)
         return;
     }
 
+    clearSelection();
+
     emit urlAboutToBeChanged(url);
     m_url = url;
 
@@ -628,6 +630,7 @@ void DolphinView::invertSelection()
 
 void DolphinView::clearSelection()
 {
+    m_selectedUrls.clear();
     m_container->controller()->selectionManager()->clearSelection();
 }
 
@@ -1211,20 +1214,18 @@ void DolphinView::updateViewState()
     }
 
     if (!m_selectedUrls.isEmpty()) {
-        clearSelection();
-
         KItemListSelectionManager* selectionManager = m_container->controller()->selectionManager();
         QSet<int> selectedItems = selectionManager->selectedItems();
 
-        foreach (const KUrl& url, m_selectedUrls) {
-            const int index = m_model->index(url);
+        for (QList<KUrl>::iterator it = m_selectedUrls.begin(); it != m_selectedUrls.end(); ++it) {
+            const int index = m_model->index(*it);
             if (index >= 0) {
                 selectedItems.insert(index);
+                m_selectedUrls.erase(it);
             }
         }
 
         selectionManager->setSelectedItems(selectedItems);
-        m_selectedUrls.clear();
     }
 }
 

KDE/kfind

commit 1948edcfeed8dbf8e19b63dee47a37e0b1b3a871

Clear the previous selection when selecting pasted or dropped items
This fixes a recent regression in the 4.9 branch caused by
b25059e803e7eed54f0593605a4066882d63bce2.

BUG: 310365
FIXED-IN: 4.9.4
REVIEW: 107389

dolphin/src/views/dolphinview.cpp
@@ -101,6 +101,7 @@ DolphinView::DolphinView(const KUrl& url, QWidget* parent) :
     m_scrollToCurrentItem(false),
     m_restoredContentsPosition(),
     m_selectedUrls(),
+    m_clearSelectionBeforeSelectingNewItems(false),
     m_versionControlObserver(0)
 {
     m_topLayout = new QVBoxLayout(this);
@@ -1218,6 +1219,12 @@ void DolphinView::updateViewState()
 
     if (!m_selectedUrls.isEmpty()) {
         KItemListSelectionManager* selectionManager = m_container->controller()->selectionManager();
+
+        if (m_clearSelectionBeforeSelectingNewItems) {
+            selectionManager->clearSelection();
+            m_clearSelectionBeforeSelectingNewItems = false;
+        }
+
         QSet<int> selectedItems = selectionManager->selectedItems();
 
         for (QList<KUrl>::iterator it = m_selectedUrls.begin(); it != m_selectedUrls.end(); ++it) {
@@ -1535,6 +1542,7 @@ void DolphinView::markPastedUrlsAsSelected(const QMimeData* mimeData)
         destUrls << destination;
     }
     markUrlsAsSelected(destUrls);
+    m_clearSelectionBeforeSelectingNewItems = true;
 }
 
 void DolphinView::updateWritableState()

dolphin/src/views/dolphinview.h
@@ -759,6 +759,7 @@ private:
     QPoint m_restoredContentsPosition;
 
     QList<KUrl> m_selectedUrls; // Used for making the view to remember selections after F5
+    bool m_clearSelectionBeforeSelectingNewItems;
 
     VersionControlObserver* m_versionControlObserver;
 