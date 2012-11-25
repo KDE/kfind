KDE/kde-baseapps

commit 4120805872c2e9fac909a403b83221c09c9110a2

Update the 'isCut' state of items when cutting non-files, e.g., text
BUG: 304615
FIXED-IN: 4.9.4
REVIEW: 107390

dolphin/src/kitemviews/private/kfileitemclipboard.cpp
@@ -55,12 +55,8 @@ KFileItemClipboard::~KFileItemClipboard()
 void KFileItemClipboard::updateCutItems()
 {
     const QMimeData* mimeData = QApplication::clipboard()->mimeData();
-    const QByteArray data = mimeData->data("application/x-kde-cutselection");
-    const bool isCutSelection = (!data.isEmpty() && data.at(0) == QLatin1Char('1'));
-    if (isCutSelection) {
-        m_cutItems = KUrl::List::fromMimeData(mimeData).toSet();
-        emit cutItemsChanged();
-    }
+    m_cutItems = KUrl::List::fromMimeData(mimeData).toSet();
+    emit cutItemsChanged();
 }
 
 KFileItemClipboard::KFileItemClipboard() :

KDE/kfind

commit 3ac7b2cb66c098d3cc45e92c1742914827b16b1f

Fix regression caused by 4120805872c2e9fac909a403b83221c09c9110a2
Copied items should not be shown in gray. Thanks to Christoph Feck for
testing and reporting this regression!

CCBUG: 304615
CCMAIL: christoph@maxiom.de

dolphin/src/kitemviews/private/kfileitemclipboard.cpp
@@ -55,7 +55,13 @@ KFileItemClipboard::~KFileItemClipboard()
 void KFileItemClipboard::updateCutItems()
 {
     const QMimeData* mimeData = QApplication::clipboard()->mimeData();
-    m_cutItems = KUrl::List::fromMimeData(mimeData).toSet();
+    const QByteArray data = mimeData->data("application/x-kde-cutselection");
+    const bool isCutSelection = (!data.isEmpty() && data.at(0) == QLatin1Char('1'));
+    if (isCutSelection) {
+        m_cutItems = KUrl::List::fromMimeData(mimeData).toSet();
+    } else {
+        m_cutItems.clear();
+    }
     emit cutItemsChanged();
 }
 