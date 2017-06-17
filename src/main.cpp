758cb9b40bfb8834eb21f242e2c5db4e76b317a1
Montel Laurent <montel@kde.org>
2017-06-17 21:03:10 +0200

    Update copyright

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,15 @@
 
 int main( int argc, char ** argv )
 {
+    QApplication app(argc, argv);
     Kdelibs4ConfigMigrator migrate(QStringLiteral("kfind"));
     migrate.setConfigFiles(QStringList() << QStringLiteral("kfindrc"));
     migrate.migrate();
-    QApplication app(argc, argv);
     KLocalizedString::setApplicationDomain("kfind");
 
   KAboutData aboutData( QLatin1String("kfind"), i18n("KFind"),
       QLatin1String(KFIND_VERSION_STRING), i18n("KDE file find utility"), KAboutLicense::GPL,
-      i18n("(c) 1998-2016, The KDE Developers"));
+      i18n("(c) 1998-2017, The KDE Developers"));
 
   aboutData.addAuthor(i18n("Kai-Uwe Broulik"), i18n("Current Maintainer"), QLatin1String("kde@privat.broulik.de"));
   aboutData.addAuthor(i18n("Eric Coquelle"), i18n("Former Maintainer"), QLatin1String("coquelle@caramail.com"));
